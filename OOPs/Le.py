078783a85c17ab328f4a14bd1d256114e82e735a
my name <janeeshpofficial@gmail.com>
2024-05-30 03:55:29 +0530

    a

---
+++ OOPs/Le.py
@@ -0,0 +1,12 @@
+class Jane:
+    pass
+    
+
+c1=Jane()
+c2=Jane()
+c3=Jane()
+print(id(c1))
+print(id(c2))
+print(id(c3))
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+class Computer:
+    def __init__(self):
+        self.name = "Janeesh"
+        self.age = 25
+    def update(self):
+        self.age = 30
+
+c1=Computer()
+c2=Computer()
+print(c1.age)
+c1.update()
+print(c1.age)
+if
